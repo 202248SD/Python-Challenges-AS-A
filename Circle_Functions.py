b1fd0b39ce5f185f26f6b54285b6bb6c6bcc46f1
Shunto Dohi <112991989+202248SD@users.noreply.github.com>
2023-02-10 18:49:28 +0800

    Create Circle_Functions.py

---
+++ Circle_Functions.py
@@ -0,0 +1,10 @@
+a = int(input("Put the radius of the circle"))
+def area():
+  area = a**2 * 3.14
+  print("The area of the circle is",area,"units^2")
+def circumference():
+  circumference = 2 * a * 3.14
+  print("The circumference is",circumference,"units")
+area()
+circumference()
+#AS80
